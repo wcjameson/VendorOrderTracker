cfbef726eb4de36f0ee8b705c1f465ac0cf77c90
wcjameson <williamjameson90@gmail.com>
2022-03-04 09:24:38 -0800

    add tests for constructor behavior

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -15,8 +15,26 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      string description = "test";
+      Order newOrder1 = new Order(description);
+      string result = newOrder1.Description;
+      Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "test";
+      Order newOrder2 = new Order(description);
+      string updatedDescription = "a test string";
+      newOrder2.Description = updatedDescription;
+      string result = newOrder2.Description;
+      Assert.AreEqual(updatedDescription, result);
     }
   }
 }
