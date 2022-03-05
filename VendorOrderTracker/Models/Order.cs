77538b090b823fdfbd3a88f88012122447b862b2
wcjameson <williamjameson90@gmail.com>
2022-03-04 17:06:05 -0800

    remove comments

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,9 +5,6 @@ namespace VendorOrderTracker.Models
   public class Order
   {
     public string Description { get; set; }
-    // public string Title { get; set; }
-    // public int Price { get; set; }
-    // public string Date { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
     public static List<Order> GetAll()
@@ -21,9 +18,6 @@ namespace VendorOrderTracker.Models
     public Order(string description)
     {
       Description = description;
-      // Title = title;
-      // Price = price;
-      // Date = date;
       _instances.Add(this);
       Id = _instances.Count;
     }
